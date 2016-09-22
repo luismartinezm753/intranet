8508d3becdb0fe66d2ac0d27383ba5cf13e78c5e
luismartinezm753 <luis.martinez@ug.uchile.cl>
2016-09-22 14:16:28 -0300

    changes in composer

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -185,9 +185,9 @@ Plugin::load('Ajax');
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
-if (Configure::read('debug')) {
+/*if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
-}
+}*/
 
 /**
  * Connect middleware/dispatcher filters.
